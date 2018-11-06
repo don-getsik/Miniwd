a610119199cf24971cdb985a553d4aad1f5fc029
Rafał Rojek <rafal.rojek01@student.wat.edu.pl>
2018-11-06 21:14:12 +0100

    Init Commit

---
+++ Miniwd/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Miniwd.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Aplikacja wykorzysująca Sztuczną inteligęcję aby rekomendować mieszkania :D";
+        }
+    }
+}
