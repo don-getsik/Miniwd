b8b4d436c8d84d84558ce777912eaeb048325465
Rafał Rojek <rafal.rojek01@student.wat.edu.pl>
2018-11-09 12:43:27 +0100

    Dodanie paru elementów modelu

---
+++ Miniwd/Models/Flat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Miniwd.Models
+{
+    public class Flat
+    {
+        public string KindOfOperation { get; set; } //Czy kupno czy wynajem
+        public string KindOfSpace { get; set; } //Czy pokój czy mieszkanie
+        public int RoomsAmount { get; set; } //Liczba pokoi
+        public int BathroomsAmount { get; set; } //Liczba łazienek
+
+
+    }
+}
