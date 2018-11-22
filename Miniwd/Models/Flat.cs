fe24aaee4f7606cbfddccfe0335f888ca33fa01c
rybol2580 <rybol2580@gmail.com>
2018-11-22 23:27:05 +0100

    Dodane zostały style dla niektórych pól formatki

--- Miniwd/Models/Flat.cs
+++ Miniwd/Models/Flat.cs
@@ -12,7 +12,8 @@ namespace Miniwd.Models
         public string KindOfSpace { get; set; } //Czy pokój czy mieszkanie
         public int RoomsAmount { get; set; } //Liczba pokoi
         public int BathroomsAmount { get; set; } //Liczba łazienek
-        public string IsGoodLocation { get; set; } //Dobra lokalizacja
+        //public string IsGoodLocation { get; set; } //Dobra lokalizacja
+        public int LocationRating { get; set; } // Ocena lokalizacji
         public double Size { get; set; } //Metraż
         public decimal Price { get; set; } //Cena
         public int Standard { get; set; } //Standard mieszkania
