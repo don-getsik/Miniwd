9be7413ff34bfdf7351b934020f8f4fd1725360f
Getsik <rafalrojek96@gmail.com>
2018-11-28 22:42:25 +0100

    Rafal r (#14)
    * Dodanie paru elementów modelu
    
    * Dodanie formatki
    
    * Dodanie elementów do formatki
    
    * Dodanie odpoweidzi serwera
    
    * Added error screen
    
    * Added error screen
    
    * Dodanie validacji pól
    
    * DOdanie walidacji pól
    
    * Parę poprawek
    
    * Dodanie integracji z Python

--- Miniwd/Artificial_Intelligence/predict.py
+++ Miniwd/Artificial_Intelligence/predict.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import pickle
 import sklearn
+import sys
+import os
 
+print(os.getcwd())
 #Get a dictionary from execution arguments
 j = int((len(sys.argv)-1)/2)
 dictionary = {}
 for i in range (0,j):
 	dictionary[sys.argv[i*2+1]] = sys.argv[i*2+2]
-
+print("bla")
 #Replace City string with encoded value
-le = pickle.load(open("encoder.le",'rb'))
+le = pickle.load(open("Artificial_Intelligence\encoder.le",'rb'))
 dictionary["City"] = le.transform([dictionary["City"]])[0]
 
 #Get a dataframe from dictionary
 dataframe = pd.DataFrame(data=dictionary,index=[0])
 
 #Read AI model
-
-model = pickle.load(open("saved.model", 'rb'))
+model = pickle.load(open("Artificial_Intelligence\saved.model", 'rb'))
 
 #Predict and print result
 print(model.predict(dataframe)[0])
