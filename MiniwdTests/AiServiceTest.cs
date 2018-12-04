60d7eb784632271d1a9645e345eedaa85b912cd5
Kamil Przygrodzki <kamil.przygrodzki@gmail.com>
2018-12-04 20:49:41 +0100

    Basic AiServiceTest

---
+++ MiniwdTests/AiServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Miniwd.Models;
+using Miniwd.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniwdTests
+{
+    public class AiServiceTest
+    {
+        [TestMethod]
+        public void AllFieldsFilledOutTest()
+        {
+            var flat = new Flat()
+            {
+                KindOfOperation = "K",
+                KindOfSpace = "M",
+                RoomsAmount = 4,
+                LocationRating = 5,
+                Size = 88,
+                Price = 473088,
+                Standard = 5,
+                Floor = 3,
+                City = "Poznań",
+                UserRoomsAmount = 2,
+                UserPrice = 532224,
+                UserStandard = 1,
+                UserLocationRating = 0
+        };
+            var aiservice = new AIService(flat);
+            Boolean result = aiservice.BuyOrNot();
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
